3d849ec88673d8e42167c6c092dd5cfa4aa17d6e
Marie Plessz <mplessz@cmh.ens.fr>
2019-11-08 17:23:28 +0100

    improve foo

--- foo.R
+++ foo.R
@@ -1,10 +1,9 @@
-#`---
-#`title: "test_file"
-#`author: "Marie Plessz"
-#`date: "8 novembre 2019"
-#`output: github_document
-#`  
-#`---
+#` ---
+#`  title: "test_file"
+#` author: "Marie Plessz"
+#` date: "8 novembre 2019"
+#`output:github_document
+#` ---
 
 #` ça commence à être rigolo
 
@@ -12,3 +11,4 @@
 a <- 2 + 3
 print(a)
 
+#' est-ce bien vrai que je peux avoir du **gras**?
