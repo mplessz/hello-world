87db0d455a4671302d4b28e6aa33c125d133f255
Marie Plessz <mplessz@cmh.ens.fr>
2019-11-08 17:15:03 +0100

    cree foo.R

---
+++ foo.R
@@ -0,0 +1,15 @@
+#`---
+#`title: "test_file"
+#`author: "Marie Plessz"
+#`date: "8 novembre 2019"
+#`output:
+#`  html_document:
+#`    keep_md: true
+#`---
+
+#` ça commence à être rigolo
+
+#+ r
+a <- 2 + 3
+print(a)
+
